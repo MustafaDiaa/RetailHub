3593f9b31853474f2ec8b04b81fbc565166ba013
MustafaDiaa <MoustafaDiaa@hotmail.com>
2024-11-28 01:10:07 +0200

    Setting up RetailHub Folder Structure

---
+++ index.js
@@ -0,0 +1,18 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import dotenv from 'dotenv'
+
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+dotenv.config({ path: path.join(__dirname, './config/.env') })
+
+
+
+import express from 'express'
+import initApp from './src/index.router.js'
+const app = express()
+
+
+const port = process.env.PORT || 5000
+initApp(app ,express)
+app.listen(port, () => console.log(`RetailHub is listening on port ${port}!`))
